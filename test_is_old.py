9d29d0d6d38cccee86eac31fc732af00d64e5400
asadhikari90 <61194496+asadhikari90@users.noreply.github.com>
2023-09-20 17:30:37 +0530

    test file added

---
+++ test_is_old.py
@@ -0,0 +1,15 @@
+from is_old import is_old
+from datetime import datetime, timedelta
+
+def test_is_old():
+    # Test when the timestamp is exactly 2 months ago
+    two_months_ago = datetime.now() - timedelta(days=60)
+    assert is_old(two_months_ago.timestamp() * 1000) == True
+
+    # Test when the timestamp is less than 2 months ago
+    one_month_ago = datetime.now() - timedelta(days=30)
+    assert is_old(one_month_ago.timestamp() * 1000) == False
+
+    # Test when the timestamp is more than 2 months ago
+    three_months_ago = datetime.now() - timedelta(days=90)
+    assert is_old(three_months_ago.timestamp() * 1000) == True
